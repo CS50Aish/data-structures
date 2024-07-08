b912a3c8fd3020ab56d0ad4425ffd114d9688865
Ubuntu <labuser@ubuntu2204.ejxyfxlpt0luflh50m23wpk0xb.rx.internal.cloudapp.net>
2024-07-08 12:27:08 +0000

    Data Structures and Algorithms

---
+++ Numbers.js
@@ -0,0 +1,18 @@
+function findMissingNumber(arr){
+    const n = arr.length + 1;
+    const expectedSum = (n * (n + 1))/2; // Sum of first n natural numbers
+
+    let actualSum = 0;
+    for(let i = 0; i < arr.length; i++){
+        actualSum += arr[i];
+    }
+
+    console.log('Actual Sum: ' + actualSum);
+    console.log('Expected Sum: ' + expectedSum);
+
+    return expectedSum - actualSum;
+}
+
+const array = [1, 2, 4, 6, 3, 7, 8];
+console.log('Missing Number: ' + findMissingNumber(array));
+console.log('Found Miising: ' + findMissingNumber(array));
